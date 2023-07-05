66ef7c43440624b9ce6b208ee7f4b6f073b3dded
valmon55 <fedor_ka@mail.ru>
2023-07-05 18:59:15 +0300

    created folder Students

---
+++ WorkWithFiles/WorkWithFiles.Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkWithFiles.Task4
+{
+    [Serializable]
+    public class Student
+    { 
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StudentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Students";
+            if (!Directory.Exists(StudentsPath))
+            {
+                Directory.CreateDirectory(StudentsPath);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
