55cd8c4ada4ac58e0c055a68f6d3911c72eb68b0
valmon55 <fedor_ka@mail.ru>
2023-07-05 13:18:57 +0300

    task 3

---
+++ WorkWithFiles/WorkWithFiles.Task3/DirExt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithFiles.Task3
+{
+    public static class DirExt
+    {
+        public static long FolderSize(this DirectoryInfo folder)
+        {
+            long size = 0;
+            try
+            {
+                FileInfo[] files = folder.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    size += file.Length;
+                }
+
+                foreach (DirectoryInfo directory in folder.GetDirectories())
+                {
+                    Console.WriteLine($"Размер : {directory} = {size} байт");
+                    size += FolderSize(directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            return size;
+        }
+    }
+}
+
---
+++ WorkWithFiles/WorkWithFiles.Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWithFiles.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
